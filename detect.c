8d76b05c66d9d5f133fc021b245eab2510facab6
Jeroen Janssen <jeroen.janssen@gmail.com>
2020-04-25 11:31:30 +0200

    display default input/output device

--- detect.c
+++ detect.c
@@ -23,18 +23,30 @@ int main(int argc, char *argv[])
 	}
 
 	printf("\nPortAudio Device Information:\n");
+	printf("======================================================\n");
+
+	int defaultInput = Pa_GetDefaultInputDevice();
+	int defaultOutput = Pa_GetDefaultOutputDevice();
 
 	const PaDeviceInfo *deviceInfo;
 	for (int i=0; i<numDevices;i++)
 	{
 		deviceInfo = Pa_GetDeviceInfo(i);
-		printf("deviceId: %d (%s)\n", i, deviceInfo->name);
+		printf("deviceId: %d (%s) ", i, deviceInfo->name);
+		if (i == defaultInput) {
+			printf("| DEFAULT_INPUT ");
+		}
+		if (i == defaultOutput) {
+			printf("| DEFAULT_OUTPUT");
+		}
+		printf("\n");
         printf("\tmaxInputChannels       : %d\n", deviceInfo->maxInputChannels);
         printf("\tmaxOutputChannels      : %d\n", deviceInfo->maxOutputChannels);
         printf("\tdefaultSampleRate      : %.0f\n", deviceInfo->defaultSampleRate);
         printf("\tdefaultLowInputLatency : %.0f\n", deviceInfo->defaultLowInputLatency);
         printf("\tdefaultLowOutputLatency: %.0f\n", deviceInfo->defaultLowOutputLatency);
 	}
+	printf("======================================================\n");
 
 	err = Pa_Terminate();
 	if (err != paNoError)
